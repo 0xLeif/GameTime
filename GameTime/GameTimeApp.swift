8328f610b498fa8222f88bc4b97f667f2288c1c1
zmeriksen <zmeriksen@icloud.com>
2020-09-24 19:19:44 -0500

    Initial Commit

---
+++ GameTime/GameTimeApp.swift
@@ -0,0 +1,20 @@
+//
+//  GameTimeApp.swift
+//  GameTime
+//
+//  Created by Zach Eriksen on 9/24/20.
+//
+
+import SwiftUI
+
+@main
+struct GameTimeApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
